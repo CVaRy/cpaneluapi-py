ba3fa11aa5469d486f97c65ceb57254b106979dd
CVaRy <47139574+CVaRy@users.noreply.github.com>
2023-03-24 07:16:42 +0300

    2 Type Worker
    uapi - 1 Type
    cpaneluserapi.account - 2 Type

--- cpaneluserapi/__init__.py
+++ cpaneluserapi/__init__.py
@@ -4,30 +4,29 @@ import urllib
 import json
 from urllib.parse import urlencode
 
+class multi_account:
+    @classmethod
+    def __init__(mult,token,username,hostname):
+          mult.token = token
+          mult.hostname = hostname
+          mult.username = username
 
-class api(object):
-    def __init__(self, base_url:str, username:str, token:str):
-        self.base_url = base_url
-        self.username = username
-        self.token = token
-        self.headers = {
-            'Authorization': 'cpanel ' + f"{self.username}:{self.token}"
-        }
-    
-    def __cQuery(self,resource, kwargs,value={}):
-        """Query Cpanel API
-        https://documentation.cpanel.net/display/DD/Guide+to+UAPI
-        Parameters
-            resource: Module ( SSL , Email)
-            faction: gener_csr 
-            
-        Returns
-            JSON responses
+    def uapi(mult,resource, kwargs,value={}):
+        """ Resource - Module Name
+
+        kwars - Function Name
+
+        value - "params": value
+
+        Official API : https://documentation.cpanel.net/display/DD/Guide+to+UAPI
         """
+        headers = {
+            'Authorization': 'cpanel ' + f"{mult.username}:{mult.token}"
+        }
         try:
 
-            conn = http.client.HTTPSConnection(self.base_url, 2083)
-            conn.request('GET', f'/execute/{resource}/{kwargs}?{urlencode(value)}', headers=self.headers)
+            conn = http.client.HTTPSConnection(mult.hostname, 2083)
+            conn.request('GET', f'/execute/{resource}/{kwargs}?{urlencode(value)}', headers=headers)
             
             response = conn.getresponse()
             data = json.loads(response.read())
@@ -36,119 +35,118 @@ class api(object):
             return data
         except:
            print((f"Error fetching {resource}"))
+
+class account:     
+    @classmethod
+    def hostname(cls,value):
+        cls.hostname = value
+    @classmethod
+    def username(cls,value):
+        cls.username = value
+    @classmethod
+    def token(cls,value):
+        cls.token = value
+
     
-    ####
-    ####
-    #### SSL System Faction Generator CSR and Key
-    ####
-    ####
+    def cQuery(cls,resource, kwargs,value={}):
+        headers = {
+            'Authorization': 'cpanel ' + f"{cls.username}:{cls.token}"
+        }
+        try:
+
+            conn = http.client.HTTPSConnection(cls.hostname, 2083)
+            conn.request('GET', f'/execute/{resource}/{kwargs}?{urlencode(value)}', headers=headers)
+            
+            response = conn.getresponse()
+            data = json.loads(response.read())
+            conn.close()
+            
+            return data
+        except:
+           print((f"Error fetching {resource}"))
+
+class BlockIP:
     
-    def generatorCSR(self, domain:str, country:str, state:str, city:str, co:str,key_id:str,friendly_name):
-            """Generate SSL Certificate
+    def add_ip(ip):
+        account.cQuery(account,"BlockIP","add_ip",value={
+            "ip": ip
+        })
 
-                This function generates an SSL certificate.
 
-                    https://documentation.cpanel.net/display/DD/UAPI+Functions+-+SSL%3A%3Agenerate_csr,
-                Parameters
-                    string domain: example.com
-                    country: TR - https://www.iso.org/iso-3166-country-codes.html
-                    state: Istanbul
-                    city: Istanbul
-                    co: CVaRy LLC.
-            """
-
-            return self.__cQuery('SSL','generate_csr',{
-                'domains': domain,
-                'countryName': country,
-                'stateOrProvinceName': city,
-                'localityName': state,
-                'organizationName': co,
-                'key_id': key_id,
-                'friendly_name': friendly_name
-            })
-    def generatorKey(self, keytype:str,keysize:int,friendly_name:str):
-        """Generator SSL Key
-            API Doc: https://documentation.cpanel.net/display/DD/UAPI+Functions+-+SSL%3A%3Agenerate_key
-            Parameters
-            keytpye: system,rsa-2048,rsa-4096
-            int keysize: default(2048),4096(rsa-4096)
-            friendly_name: This parameter defaults to the key's type, creation date, and creation time. (TestKey)
-        """
-        keysize = 0 
-        return self.__cQuery('SSL','generate_key',{
-            'keytype': keytype,
-            'keysize': keysize,
-            'friendly_name': friendly_name,
+    def remove_ip(ip):
+        return account.cQuery(account,'BlockIP','remove_ip',{
+            'ip':ip
         })
     
-    def generatorRequestSSL(self,domain:str,country:str,state:str,city:str,co:str):
-        """AutoSSL Request CSR SSL System
-        Parameters
-        domain: example.com
-        country: TR ( USA, AZ, ENG )
-        state: Kocaeli
-        city: Kocaeli
-        co: Turhost Hosting Bilisim Hizmetleri
-        """
-        key_id = self.generatorKey("rsa-2048",2048,"AutoSSL System-"+domain)
-        key_id = json.dumps(key_id)
-        key_id = json.loads(key_id)
-        autossl = {}
-        autossl["key"] = key_id["data"]["text"]
-        response = self.generatorCSR(domain,country,state,city,co,key_id["data"]["id"],"AutoSSL System-"+domain)
-        autossl["csr"] = response["data"]["text"]
 
-        return autossl
-    def listcsr(self):
-        domainlist = self.__cQuery('SSL','list_csrs') # CSR List Talebi
-        domainler = {}
-        for i in range(len(domainlist["data"])): # Liste Sayısı Tespit
-            dm = domainlist["data"][i]["commonName"] # Gelen Domainleri DM değişkenine ata
-            domainler[i] = dm # Domainler Listesine DM değişkenini ekle
+class SSL():
 
-        return domainler # Fonsiyon döngüsü Json domain listesini ver.
+    def generatorCSR(domain:str, country:str, state:str, city:str, co:str,key_id:str,friendly_name):
+               
+                return account.cQuery(account,'SSL','generate_csr',{
+                    'domains': domain,
+                    'countryName': country,
+                    'stateOrProvinceName': city,
+                    'localityName': state,
+                    'organizationName': co,
+                    'key_id': key_id,
+                    'friendly_name': friendly_name
+                })
+    def generatorKey(keytype:str,keysize:int,friendly_name:str):
 
-    def domainlist(self,type):
-        """
-        Parameters
-        type: addon_domains,sub_domains,parked_domains
-        """
-        domainlist = self.__cQuery('DomainInfo','list_domains')
-        domainler = {}
-        if type == "main_domain":
-            return domainlist["data"][type]
-        else:
+            keysize = 0 
+            return account.cQuery(account,'SSL','generate_key',{
+                'keytype': keytype,
+                'keysize': keysize,
+                'friendly_name': friendly_name,
+            })
+        
+    def generatorRequestSSL(domain:str,country:str,state:str,city:str,co:str):
 
-            for i in range(len(domainlist["data"][type])): # Liste Sayısı Tespit
-                dm = domainlist["data"][type][i] # Gelen Domainleri DM değişkenine ata
+            key_id = SSL.generatorKey("rsa-2048",2048,"AutoSSL System-"+domain)
+            key_id = json.dumps(key_id)
+            key_id = json.loads(key_id)
+            autossl = {}
+            autossl["key"] = key_id["data"]["text"]
+            response = SSL.generatorCSR(domain,country,state,city,co,key_id["data"]["id"],"AutoSSL System-"+domain)
+            autossl["csr"] = response["data"]["text"]
+
+            return autossl
+    def listcsr():
+            domainlist = account.cQuery(account,'SSL','list_csrs') # CSR List Talebi
+            domainler = {}
+            for i in range(len(domainlist["data"])): # Liste Sayısı Tespit
+                dm = domainlist["data"][i]["commonName"] # Gelen Domainleri DM değişkenine ata
                 domainler[i] = dm # Domainler Listesine DM değişkenini ekle
 
-            return domainler
-    def BlockIP_add_ip(self,ip):
-
-        return self.__cQuery('BlockIP','add_ip',{
-            'ip':ip
-        })
-    def BlockIP_remove_ip(self,ip):
-
-        return self.__cQuery('BlockIP','remove_ip',{
-            'ip':ip
-        })
+            return domainler # Fonsiyon döngüsü Json domain listesini ver.
     
-    def Quota_get_quota_info(self):
-
-        return self.__cQuery('Quota','get_quota_info',{})
-
-    def ResourceUsage_get_usages(self):
-
-        return self.__cQuery('ResourceUsage','get_usages',{})
-
-    def findkey(self,key_id):
-        
-
-        return self.__cQuery('SSL','find_csrs_for_key',{
+    def findkey(key_id):
+        return account.cQuery(account,'SSL','find_csrs_for_key',{
             'id': key_id,
         })
-
-
-
+    def delete_cert(id:str,friendly_name:None):
+         return account.cQuery(account,"SSL","delete_cert",value={
+              "id":id,
+              "friendly_name":friendly_name
+         })
+    def delete_csr(id:str,friendly_name:None):
+         return account.cQuery(account,"SSL","delete_csr",value={
+              "id":id,
+              "friendly_name":friendly_name
+         })
+    def delete_key(id:str,friendly_name:None):
+         return account.cQuery(account,"SSL","delete_key",value={
+              "id":id,
+              "friendly_name":friendly_name
+         })
+    def delete_ssl(domain:str):
+         return account.cQuery(account,"SSL","delete_ssl",value={
+              "id":id,
+         })
+    def show_csr(domain:str,friendly_name:None):
+         return account.cQuery(account,"SSL","show_csr",value={
+              "id":id,
+              "friendly_name":friendly_name
+         })
+    
